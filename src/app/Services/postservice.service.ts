1d56d736fbb622494cc222fbb2967cca2a261100
unknown <kanishkabc123@gmail.com>
2022-07-15 23:50:22 +0530

    Observe different types of response in HttpClinet and Chnaging the response Body Type

--- src/app/Services/postservice.service.ts
+++ src/app/Services/postservice.service.ts
@@ -7,44 +7,42 @@ import { Posts } from '../posts/posts.model';
   providedIn: 'root',
 })
 export class PostserviceService {
- 
-  constructor(private httpClient: HttpClient) {}
+
+  constructor(private httpClient: HttpClient) { }
 
   addPost(postdata: { [key: string]: Posts }) {
-
-
     return this.httpClient.post<{ [key: string]: Posts }>(
       'https://ng-complete-guide-2abc1-default-rtdb.firebaseio.com/post.json',
-      postdata,{
-        headers:new HttpHeaders({
-          'custom-header':'Post Request'
-        }),
-        params: new HttpParams().set('custom','hai'),
-        observe: 'response'
-      }
+      postdata, {
+      headers: new HttpHeaders({
+        'custom-header': 'Post Request'
+      }),
+      params: new HttpParams().set('custom', 'hai'),
+      observe: 'response'
+    }
     );
   }
 
   getPosts() {
     let searchParams = new HttpParams();
-    searchParams =searchParams.append('custom','hai')
-    searchParams =searchParams.append('name','kamishka')
+    searchParams = searchParams.append('custom', 'hai')
+    searchParams = searchParams.append('name', 'kamishka')
 
-  
+
     return this.httpClient
       .get<{ [key: string]: Posts }>(
-        'https://ng-complete-guide-2abc1-default-rtdb.firebaseio.com/post.json',{
-          headers:new HttpHeaders({
-            'custom-header':'Get Request'
-          }),
-          params: new HttpParams().set('custom','hai')
-        }
+        'https://ng-complete-guide-2abc1-default-rtdb.firebaseio.com/post.json', {
+        headers: new HttpHeaders({
+          'custom-header': 'Get Request'
+        }),
+        params: new HttpParams().set('custom', 'hai')
+      }
       )
       .pipe(
         map((response) => {
           let posts = [];
           for (let key in response) {
-            posts.push({ ...response[key], key });
+            posts.push({ ...response[key], id: key });
           }
           return posts;
         })
@@ -54,26 +52,26 @@ export class PostserviceService {
   clearPosts() {
     return this.httpClient
       .delete(
-        'https://ng-complete-guide-2abc1-default-rtdb.firebaseio.com/post.json',{
-          headers:new HttpHeaders({
-            'custom-header':'Delete Request'
-          }),
-          params: new HttpParams().set('custom','hai'),
-          observe: 'events',
-          responseType:'text'
-        }).pipe(tap(response=>{
-          console.log(response);
-          if(response.type === HttpEventType.Sent){
-console.log('request sent');
+        'https://ng-complete-guide-2abc1-default-rtdb.firebaseio.com/post.json', {
+        headers: new HttpHeaders({
+          'custom-header': 'Delete Request'
+        }),
+        params: new HttpParams().set('custom', 'hai'),
+        observe: 'events',
+        responseType: 'text'
+      }).pipe(tap(response => {
+        console.log(response);
+        if (response.type === HttpEventType.Sent) {
+          console.log('request sent');
 
-          }
-          
-          if(response.type===  HttpEventType.Response){
-           console.log(response.body);
-console.log(response.body);
-            
-          }
-        }))
+        }
+
+        if (response.type === HttpEventType.Response) {
+          console.log(response.body);
+          console.log(response.body);
+
+        }
+      }))
       .subscribe((response) => {
         console.log(response);
       });
