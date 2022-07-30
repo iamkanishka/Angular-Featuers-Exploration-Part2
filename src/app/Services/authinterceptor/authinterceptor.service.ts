84d630fe3dae5741f474fd5cb614ba78ff381581
KanishkaNaik <kanishkabc123@gmail.com>
2022-07-30 16:49:54 +0530

    Manipulating Request Objects, headers with Interceptors in the angular

--- src/app/Services/authinterceptor/authinterceptor.service.ts
+++ src/app/Services/authinterceptor/authinterceptor.service.ts
@@ -3,7 +3,19 @@ import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/c
 
 export class AuthinterceptorService implements HttpInterceptor {
      intercept(req: HttpRequest<any>, next: HttpHandler){
+      let modofiedRequest = req.clone({
+        headers:req.headers.append('auth','abc'),
+       //You can overide/change the URL
+       // url:"asdbjsd"
+
+       params:req.params.append('hai','hello world')
+      })
       console.log('Sending Request interceptor')
-         return next.handle(req)
+       //default request  
+      // return next.handle(req)
+
+      //modified Request
+         return next.handle(modofiedRequest)
+
      }
 }
