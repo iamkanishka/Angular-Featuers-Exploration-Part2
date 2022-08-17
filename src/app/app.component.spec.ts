c471aac23abdaec269edf6e5b8860f029c5f5e9e
KanishkaNaik <kanishkabc123@gmail.com>
2022-08-17 23:30:44 +0530

    Renamed Application name

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angularroutingfeatures'`, () => {
+  it(`should have as title 'Angular-Featuers-Exploration-Part2'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angularroutingfeatures');
+    expect(app.title).toEqual('Angular-Featuers-Exploration-Part2');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angularroutingfeatures app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Angular-Featuers-Exploration-Part2 app is running!');
   });
 });
